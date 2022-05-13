35f5dbe4a362bf72f86f67d7777f088e2c8b5e1e
jesiallen <justjesi2010@gmail.com>
2022-05-13 00:34:24 +0000

    finished API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,68 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/contact/all', methods=['GET'])
+def get_contacts():
+    contact_query = Contact.query.all()
+    all_people = list(map(lambda x: x.serialize(), contact_query))
+    return jsonify(all_people)
+
+@app.route('/contact', methods=['POST'])
+def add_contact():
+    body= request.get_json()
+    contact=Contact(name= body['full_name'], email=body['email'], address=body['address'], phone= body['phone'])
+    db.session.add(contact)
+    db.session.commit()
+    if body is None:
+        return "The request body is null", 400
+    elif body['full_name'] is None:
+        return "The request has no name", 400
+    else:
+        contact_query = Contact.query.all()
+        all_people = list(map(lambda x: x.serialize(), contact_query))
+        return jsonify(all_people)
+
+@app.route('/contact/<contact_id>', methods=['GET'])
+def get_contact(contact_id):
+    user1 = Contact.query.get(contact_id)
+    return jsonify(user1.serialize())
+
+@app.route('/contact/<contact_id>', methods=['DELETE'])
+def delete_contact(contact_id):
+    user1 = Contact.query.get(contact_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    contact_query = Contact.query.all()
+    all_people = list(map(lambda x: x.serialize(), contact_query))
+    return jsonify(all_people)
+
+
+@app.route('/contact/<contact_id>', methods=['PUT'])
+def edit_contact(contact_id):
+    body= request.get_json()
+    user1 = Contact.query.get(contact_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    if "full_name" in body:
+        user1.full_name = body["full_name"]
+    if "email" in body:
+        user1.email = body["email"]
+    if "phone" in body:
+        user1.phone = body["phone"]
+    if "address" in body:
+        user1.address = body["address"]
+    db.session.commit()
+    contact_query = Contact.query.all()
+    all_people = list(map(lambda x: x.serialize(), contact_query))
+    return jsonify(all_people)
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,24 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(200), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Contact %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "full_name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            "address": self.address,
+            # do not serialize the password, its a security breach
+        }
+
